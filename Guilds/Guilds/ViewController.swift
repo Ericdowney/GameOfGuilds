1621622ee52cd3a29a7bd8489833a96a9ee659a2
Downey <edowney@ICC11077.local>
2015-11-03 09:19:32 -0500

    Initial Commit + Parse w/ Carthage

---
+++ Guilds/Guilds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guilds
+//
+//  Created by Downey, Eric on 10/29/15.
+//  Copyright © 2015 ICCT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
